f790b0538c7d7600bd97389d66659718b816982b
Zhushaofeng <15738510689@163.com>
2019-02-01 13:07:41 +0800

    happyNewYear

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
     <h1>年</h1>
     <h1>快</h1>
     <h1>乐</h1>
+    <div class="imgContain">
+    </div>
+    
   </div>
 </template>
 
@@ -36,5 +39,15 @@ export default {
   }
   #app h1 {
     font-size: 100px;
+  }
+  .imgContain {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 800px;
+    background-image: url('./584533a00f2b6.jpg');
+    background-position: center center;
+    background-repeat: no-repeat;
   }
 </style>
